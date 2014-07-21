272c010ae23a61b80dd9db6fe61f33efb36cc8b5
Brett <beergeek@tpg.com.au>
2014-07-21 15:31:36 +1000

    Fix commenting errors?

--- manifests/frontend_webserver.pp
+++ manifests/frontend_webserver.pp
@@ -1,11 +1,8 @@
 class roles::frontend_webserver {
 
-<<<<<<< HEAD
   include profiles::base
   include profiles::apache_php
-=======
   include base
   include apache_php
->>>>>>> cdd09ddd6e86b29b272679fb20f7accfee29bbbf
 
 }
